7be7bd2963b0d16986ca20fc9f77377ad0de73b8
dipaktapase <dpktapase@gmail.com>
2023-02-18 13:57:31 +0530

    basic metamask setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
-function App() {
+
+export default function App() {
+
+  const [walletAddress, setWalletAddress] = useState('');
+
+  async function connectToMetamask() {
+    
+    if(window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setWalletAddress(accounts[0]);
+      } catch(e) {
+        console.log(e);
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="btn btn-primary" onClick={connectToMetamask}>
+        Connect
+      </button>
+      <h3>Wallet Address: {walletAddress}</h3>
     </div>
   );
-}
-
-export default App;
+  }
